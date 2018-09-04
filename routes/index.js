c0c8f8df3346a23c29a36bffff9ac0324eba611a
Molly <mollysarfati5@gmail.com>
2018-09-04 12:00:29 +0200

    init

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ const AWS_S3_KEY = {
                 accessKeyId: process.env.AWS_ACCESS_KEY_ID,
                 secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID
               }
+              //create a file .env 
 
 const myBucket = 'YOUR BUCKET NAME';
 
